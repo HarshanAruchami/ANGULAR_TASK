9c7e17c95be0d91d9dd3ceda7369e7cd1ce30421
HarshanAruchami <harshanaruchami@gmail.com>
2023-06-30 11:05:42 +0530

    final commit with all the changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,15 +7,41 @@ function openNav() {
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
   document.getElementById("myleftSidepanel").style.width = "0";
+  document.getElementById("myleftSidepanel").style.transition = "width 0.1s";
 }
+document
+  .getElementById("myleftSidepanel")
+  .addEventListener("onclick", function () {
+    this.style.width = "250px";
+  });
 
-// SIDEPANEL JS FOR SETTINGS
-/* Set the width of the sidebar to 250px (show it) */
+document
+  .getElementById("myleftSidepanel")
+  .addEventListener("mouseleave", function () {
+    this.style.width = "0";
+  });
+
+// SIDEPANEL JS FOR HOVER SETTINGS BUTTON
 function openSet() {
   document.getElementById("myrightSidepanel").style.width = "230px";
+  document.getElementById("myrightSidepanel").style.transition = "width 0.1s";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeSet() {
   document.getElementById("myrightSidepanel").style.width = "0";
+  document.getElementById("myrightSidepanel").style.transition = "width 0.1s";
 }
+
+/* Add hover effect to the sidebar */
+document
+  .getElementById("myrightSidepanel")
+  .addEventListener("mouseenter", function () {
+    this.style.width = "250px";
+  });
+
+document
+  .getElementById("myrightSidepanel")
+  .addEventListener("mouseleave", function () {
+    this.style.width = "0";
+  });
